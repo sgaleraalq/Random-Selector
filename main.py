5623571fb793c9a0509018a0ad1e9af48a51ec5f
Sergio Galera <sgaleraalq@gmail.com>
2024-04-19 12:06:03 +0200

    It prints in green every 1.5 seconds random characters

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class RandomSelector():
     def __init__(self, items):
         self.items          = items
         self.selected       = random.choice(self.items)
-        self.colors         = {"red": 1, "green": 2, "reset": 0}
+        self.colors         = {"black": 1, "green": 2}
         # self.console_out    = curses.initscr()
         # self.height         = self.console_out.getmaxyx()[0]-2
         # self.lines          = int(self.height / 2) if self.height % 2 == 0 else int((self.height + 1) / 2)
@@ -33,16 +33,34 @@ class RandomSelector():
             line_position = index*2
             self.console_out.addstr(line_position, 0, "banana")
 
+    def get_random(self, lst: list):
+        index = random.choice(lst)
+        lst.remove(index)
+        return lst, index
+
 
     def show_result(self):
-        remaining_indices = list(range(len(self.selected)))
+        console_out = curses.initscr()
+        curses.start_color()
 
-        while remaining_indices:
-            to_be_added = random.choice(remaining_indices)
-            print(to_be_added)
-            remaining_indices.remove(to_be_added)
-            time.sleep(1)
-        print(self.selected)
+        curses.init_pair(self.colors["black"], curses.COLOR_BLACK, curses.COLOR_BLACK)
+        curses.init_pair(self.colors["green"], curses.COLOR_GREEN, curses.COLOR_BLACK)
+
+        all_indices = list(range(len(self.selected)))
+
+        try:
+            console_out.attron(curses.color_pair(self.colors["black"]))
+            console_out.refresh()
+
+            while all_indices:
+                all_indices, index = self.get_random(all_indices)
+                console_out.attron(curses.color_pair(self.colors["green"]))
+                console_out.addstr(curses.LINES - 1, index, self.selected[index])
+                console_out.attroff(curses.color_pair(self.colors["green"]))
+                console_out.refresh()
+                time.sleep(1.5)
+        finally:
+            curses.endwin()
 
 selector = RandomSelector(["apple", "banana", "orange"])
 # selector.print_result

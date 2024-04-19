885a3f92cf478a90a7a5f04efac06a51558f9d7c
Sergio Galera <sgaleraalq@gmail.com>
2024-04-19 14:18:49 +0200

    Trying with threading

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 
 import random
 import curses
+import threading
 
 class RandomSelector():
     def __init__(self, items):
@@ -12,7 +13,11 @@ class RandomSelector():
         self.height             = self.console_out.getmaxyx()[0]-2
         self.lines              = int(self.height / 2) if self.height % 2 == 0 else int((self.height + 1) / 2)
         self.lines_list         = ["" for _ in range(self.lines)]
-        self.letters_selected   = ["" for _ in range(len(self.selected))]
+        self.selected_letters   = ["" for _ in range(len(self.selected))]
+        self.all_indices        = list(range(len(self.selected)))
+        self.interval_x         = 1
+        self.interval_y         = 0.1
+        self.stop_event = threading.Event()
 
         curses.start_color()
         curses.init_pair(self.colors["black"], curses.COLOR_BLACK, curses.COLOR_BLACK)
@@ -27,64 +32,54 @@ class RandomSelector():
         lst.remove(index)
         return lst, index
 
+    def initialize(self):
+        self.all_indices, index = self.get_random(self.all_indices)
+        self.selected_letters[index] = self.selected[index]
+        return
 
-    def all_choices(self, total_time: float):
-        start_time = time.time()
-        end_time = start_time + total_time
-        
-        while time.time() < end_time:
-            new_choice = random.choice(self.items)
-            self.lines_list.pop(0)
-            self.lines_list.append(new_choice)
-            self.console_out.clear()
-            self.console_out.addstr(curses.LINES - 1, 0, self.selected)
+    def main(self):
+        self.initialize()
+        thread_x = threading.Thread(target=self.print_all_choices())
+        thread_y = threading.Thread(target=self.print_selected())
 
-            for index, choice in enumerate(self.lines_list):
-                for letter in range(len(choice)):
+        thread_x.start()
+        thread_y.start()
 
-                    if letter <= len(self.letters_selected) - 1 and self.letters_selected[letter] == choice[letter]:
-                        self.console_out.attron(curses.color_pair(self.colors["green"]))
-                        self.console_out.addstr(index * 2, letter, choice[letter])
-                        self.console_out.attroff(curses.color_pair(self.colors["green"]))
-                    
-                    else:
-                        self.console_out.addstr(index * 2, letter, choice[letter])
-            
-            ## Print last line
-            # for letter in range(len(self.letters_selected)):
-            #     if self.letters_selected[letter] != "":
-            #         self.console_out.attron(curses.color_pair(self.colors["green"]))
-            #         self.console_out.addstr(curses.LINES - 1, letter, self.letters_selected[letter])
-            #         self.console_out.attroff(curses.color_pair(self.colors["green"]))
+        try:
+            while True:
+                time.sleep(0.1)
 
-
-            self.console_out.refresh()
-            time.sleep(1)
+        except KeyboardInterrupt:
+            self.stop_event.set()
+            thread_x.join()
+            thread_y.join()
 
         if self.console_out.getch():
             self.stop_curses
 
-    def show_result(self, time_per_letter: float):
-        all_indices = list(range(len(self.selected)))
-        self.all_choices(time_per_letter * len(self.selected))
+    def print_all_choices(self):
+        while not self.stop_event.is_set():
+            for index, word in enumerate(self.lines_list):
+                for letter in range(len(word)):
+                    if letter <= len(self.selected_letters) - 1 and self.selected_letters[letter] == word[letter]:
+                        self.console_out.attron(curses.color_pair(self.colors["green"]))
+                        self.console_out.addstr(index * 2, letter, word[letter])
+                        self.console_out.attroff(curses.color_pair(self.colors["green"]))
+                    else:
+                        self.console_out.addstr(index * 2, letter, word[letter])
+            time.sleep(self.interval_y)
+        return
 
-        try:
-            self.console_out.attron(curses.color_pair(self.colors["black"]))
-            self.console_out.refresh()
-
-            while all_indices:
-                all_indices, index = self.get_random(all_indices)
-                self.letters_selected[index] = self.selected[index]
-                self.console_out.attron(curses.color_pair(self.colors["green"]))
-                self.console_out.addstr(curses.LINES - 1, index, self.selected[index])
-                self.console_out.attroff(curses.color_pair(self.colors["green"]))
-                self.console_out.refresh()
-                time.sleep(time_per_letter)
-            self.console_out.getch()
-
-        finally:
-            curses.endwin()
+    def print_selected(self):
+        while not self.stop_event.is_set():
+            for letter in range(len(self.selected_letters)):
+                if self.selected_letters[letter] != "":
+                    self.console_out.attron(curses.color_pair(self.colors["green"]))
+                    self.console_out.addstr(curses.LINES - 1, letter, self.selected_letters[letter])
+                    self.console_out.attroff(curses.color_pair(self.colors["green"]))
+            time.sleep(self.interval_x)
+        return
 
 selector = RandomSelector(["apple", "banana", "orange", "grape", "kiwi", "mango", "pear", "peach", "plum", "strawberry"])
-selector.show_result(0.5)
+selector.main()
 # selector.all_choices(2)

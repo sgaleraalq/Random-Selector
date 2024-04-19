5379cfc5630e85098347e80147d711316dbcdfb5
Sergio Galera <sgaleraalq@gmail.com>
2024-04-19 14:29:08 +0200

    Some updates but missing threading to work

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-import time
-
 import random
 import curses
 import threading
+import time
 
 class RandomSelector():
     def __init__(self, items):
         self.items              = items
         self.selected           = random.choice(self.items)
         self.colors             = {"black": 1, "green": 2}
-        self.console_out        = curses.initscr()
-        self.height             = self.console_out.getmaxyx()[0]-2
-        self.lines              = int(self.height / 2) if self.height % 2 == 0 else int((self.height + 1) / 2)
-        self.lines_list         = ["" for _ in range(self.lines)]
-        self.selected_letters   = ["" for _ in range(len(self.selected))]
-        self.all_indices        = list(range(len(self.selected)))
+        self.console_out        = None 
+        self.height             = None
+        self.lines              = None
+        self.lines_list         = None
+        self.selected_letters   = None
+        self.all_indices        = None
         self.interval_x         = 1
         self.interval_y         = 0.1
-        self.stop_event = threading.Event()
+        self.stop_event         = threading.Event()
+
+    def initialize(self):
+        self.console_out = curses.initscr()
+        self.height = self.console_out.getmaxyx()[0]-2
+        self.lines = int(self.height / 2) if self.height % 2 == 0 else int((self.height + 1) / 2)
+        self.lines_list = ["" for _ in range(self.lines)]
+        self.selected_letters = ["" for _ in range(len(self.selected))]
+        self.all_indices = list(range(len(self.selected)))
 
         curses.start_color()
         curses.init_pair(self.colors["black"], curses.COLOR_BLACK, curses.COLOR_BLACK)
@@ -32,15 +39,10 @@ class RandomSelector():
         lst.remove(index)
         return lst, index
 
-    def initialize(self):
-        self.all_indices, index = self.get_random(self.all_indices)
-        self.selected_letters[index] = self.selected[index]
-        return
-
     def main(self):
         self.initialize()
-        thread_x = threading.Thread(target=self.print_all_choices())
-        thread_y = threading.Thread(target=self.print_selected())
+        thread_x = threading.Thread(target=self.print_all_choices)
+        thread_y = threading.Thread(target=self.print_selected)
 
         thread_x.start()
         thread_y.start()
@@ -53,9 +55,7 @@ class RandomSelector():
             self.stop_event.set()
             thread_x.join()
             thread_y.join()
-
-        if self.console_out.getch():
-            self.stop_curses
+            self.stop_curses  # Detiene curses
 
     def print_all_choices(self):
         while not self.stop_event.is_set():
@@ -82,4 +82,3 @@ class RandomSelector():
 
 selector = RandomSelector(["apple", "banana", "orange", "grape", "kiwi", "mango", "pear", "peach", "plum", "strawberry"])
 selector.main()
-# selector.all_choices(2)

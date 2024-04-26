13873c1685c954eb32b61c9d3db4d53070f42ed1
Sergio Galera <sgaleraalq@gmail.com>
2024-04-26 15:13:15 +0200

    Some function creation

--- groups.py
+++ groups.py
@@ -1,5 +1,21 @@
 from main import RandomSelector
+import curses
 
 groups = ["Denmark", "England", "France"]
 
 group = RandomSelector(groups)
+
+class GroupSelector():
+    def __init__(self, groups, participants):
+        self.groups = groups
+        self.participants = participants
+        self.console_out = curses.initscr()
+    
+
+    # On exit, stop curses
+    @property
+    def stop_curses(self):
+        curses.endwin()
+    
+
+## If exit, stop curses
